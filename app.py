8d2307c6e26903a9e7f35140dad587bab828ec40
Oksibib <pierrechaibi@gmail.com>
2026-07-17 09:43:57 +0200

    Refactor app.py to include db_init import

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 from db import *
+import db_init
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from functools import wraps
